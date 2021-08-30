7df4a62ed6304e581e6b13e4b384e42962d2105d
Zain Ul abideen <80303452+zainulabdn@users.noreply.github.com>
2021-08-30 23:54:08 +0500

    changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:ecom_graphql/main.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:graphql/client.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,20 +12,21 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("GraphQl"),
       ),
+      // body: Text('aaa'),
       body: Query(
         options: QueryOptions(
           document: gql(productsGraphQL),
         ),
         builder: (QueryResult result, {fetchMore, refetch}) {
           if (result.hasException) {
-            return Text(result.hasException.toString());
+            return Text('Exception: ${result.hasException.toString()}');
           }
           if (result.isLoading) {
             return Center(
               child: CircularProgressIndicator(),
             );
           }
-          final productList = result.data?['characters']['results'];
+          List productList = result.data?['collections']['edges']['node'];
           print(productList);
           return Column(
             children: [
@@ -53,13 +55,13 @@ class Home extends StatelessWidget {
                           height: 180,
                           width: 180,
                           // child: Text(products['image'].toString()),
-                          child: Image.network(products['image']),
+                          // child: Image.network(products['image']),
                         ),
                         Text(
-                          products['name'],
+                          products['title'],
                           style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        Text(products['status'])
+                        // Text(products['moneyFormat'])
                       ],
                     );
                   },
@@ -72,3 +74,79 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
+// import 'package:ecom_graphql/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
+
+// class Home extends StatelessWidget {
+//   const Home({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("GraphQl"),
+//       ),
+//       body: Query(
+//         options: QueryOptions(
+//           document: gql(productsGraphQL),
+//         ),
+//         builder: (QueryResult result, {fetchMore, refetch}) {
+//           if (result.hasException) {
+//             return Text(result.hasException.toString());
+//           }
+//           if (result.isLoading) {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//           final productList = result.data?['shop'];
+//           print(productList);
+//           return Column(
+//             children: [
+//               Padding(
+//                 padding: EdgeInsets.all(19.0),
+//                 child: Text(
+//                   "Products",
+//                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//                 ),
+//               ),
+//               Expanded(
+//                 child: GridView.builder(
+//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                     crossAxisCount: 2,
+//                     mainAxisSpacing: 2.0,
+//                     crossAxisSpacing: 2.0,
+//                     childAspectRatio: 0.75,
+//                   ),
+//                   itemCount: productList.length,
+//                   itemBuilder: (_, index) {
+//                     var products = productList[index]['shop'];
+//                     return Column(
+//                       children: [
+//                         Container(
+//                           padding: EdgeInsets.all(2.0),
+//                           height: 180,
+//                           width: 180,
+//                           // child: Text(products['image'].toString()),
+//                           // child: Image.network(products['onlineStoreUrl']),
+//                         ),
+//                         Text(
+//                           products['name'],
+//                           style: TextStyle(fontWeight: FontWeight.bold),
+//                         ),
+//                         // Text(products['title']),
+//                       ],
+//                     );
+//                   },
+//                 ),
+//               )
+//             ],
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:ecom_graphql/home.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:graphql/client.dart';
 
 const productsGraphQL = """
-query {
-   characters{
-     results{
-      id
-      name
-      image
-      status
+{
+  collections(first:5){
+    edges{
+      node{
+        id
+        title
+      }
     }
   }
 }
 """;
 
-void main() {
-  final HttpLink httpLink = HttpLink("https://rickandmortyapi.com/graphql");
+void main() async {
+  // await initHiveForFlutter();
+  final HttpLink httpLink =
+      HttpLink("https://ewiglife-ecomapp.myshopify.com/api/graphql");
+
+  // ignore: unused_local_variable
+  final AuthLink authLink = AuthLink(
+      headerKey: 'X-Shopify-Storefront-Access-Token',
+      getToken: () async => '86338fd3004e0c485ca61f68dd2c59e7');
+  // ignore: unused_local_variable
+  final Link link = authLink.concat(httpLink);
   ValueNotifier<GraphQLClient> client = ValueNotifier(GraphQLClient(
       link: httpLink, cache: GraphQLCache(store: InMemoryStore())));
 
